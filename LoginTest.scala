edec8b3808ab54946a377c900ce12aaf4d7c5c6e
antonio-iannotta <91571686+antonio-iannotta@users.noreply.github.com>
2022-06-30 21:55:13 +0200

    Create LoginTest.scala

---
+++ LoginTest.scala
@@ -0,0 +1,29 @@
+package login
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.collection.mutable.LinkedHashMap
+
+class LoginTest extends AnyFunSuite:
+
+  val userMap: LinkedHashMap[String, String] = LinkedHashMap("User1" -> "Password1", "User2" -> "Password2")
+
+  test("User1 is not blank") {
+    val user = Login.signIN("User1","Password1")
+    assert(user.get.getUserID == "User1" && user.get.getPassword == "Password1")
+  }
+
+  test("User is blank") {
+    val user = Login.signIN(" ","Password1")
+    assert(user.get.getUserID == "")
+  }
+
+  test("Password is not blank") {
+    val user = Login.signIN("User1","Password1")
+    assert(user.get.getPassword == "Password1")
+  }
+
+  test("Password is blank"){
+    val user = Login.signIN("User1","")
+    assert(user.get.getPassword == "")
+  }
