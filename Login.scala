65777fd1cf0680ad4683a61c246c1bf34831d747
antonio-iannotta <91571686+antonio-iannotta@users.noreply.github.com>
2022-06-30 21:47:25 +0200

    Create Login.scala

---
+++ Login.scala
@@ -0,0 +1,18 @@
+package login
+
+import mongoDriver.MongoDB._
+import registration.MD5
+import user.User
+
+object Login:
+  def signIN(userID: String, password: String): Option[User] =
+    var user: Option[User] = Option.empty
+    var checkerResponse: String = ""
+    val loginChecker = LoginChecker(userID, password)
+    
+    checkerResponse = loginChecker.checkFields(userID, password)
+    if checkerResponse == "OK" then
+      val userList = retrieveUsers()
+      user = Option.apply(userList.filter(user => user.getUserID() == userID && user.getPassword() == MD5.md5HashPassword(password)).head)
+
+    user
